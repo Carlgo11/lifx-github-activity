acf1ed5b26ae309244440d78df844bc1be14d1c7
Carlgo11 <Carlgo11@users.noreply.github.com>
2020-08-29 02:14:34 +0200

    Use GitHub native heatmap instead of RSS feed

--- feed.rb
+++ feed.rb
@@ -1,56 +1,44 @@
 #! /usr/bin/env ruby
-# frozen_string_literal: true
-
 require 'nokogiri'
 require 'open-uri'
 require 'date'
 
-if ENV['GITHUB_USER'].nil?
-  abort("Error: Set environment variable 'GITHUB_USER'!")
+a = Hash.new { |h, k| h[k] = '' }
+
+for k in 0..4 do
+  a.store((Date.today - k).strftime('%Y-%m-%d'), 0)
 end
 
-begin
-  doc = Nokogiri::HTML(open("https://github.com/#{ENV['GITHUB_USER']}.atom"))
-rescue OpenURI::HTTPError => e
-  abort("Error: 404 - Couldn't fetch user activity. Double check the spelling?") if e.message == '404 Not Found'
-  raise e
+document = Nokogiri::HTML.parse(URI.open("https://github.com/#{ENV['GITHUB_USER']}"))
+
+# Get last 5 items in array
+document.css('.day').each do |item|
+  next if (Date.today - Date.iso8601(item['data-date'])) >= 5
+  a.store(item['data-date'], Integer(item['data-count']))
 end
 
-active_days = Array.new(5, 0)
-
-doc.css('feed entry published').each do |entry|
-  diff = Date.today - Date.iso8601(entry.content)
-  break if diff >= 5
-
-  active_days[diff] += 1
-end
+# New list setting the color range
+c = Hash.new { |h, k| h[k] = '' }
 
 # Get max commits
-max = active_days.max
-output = []
+max = a.sort_by { |_key, value| value }.to_a.last[1]
 
-if max.zero?
-  output = Array.new(5, 0)
-else
-  # Set color level for each day
-  active_days.each do |value|
-    output = case value
-             when max * 0.8..max
-               [*output, 4]
-             when max * 0.65..max * 0.8
-               [*output, 3]
-             when max * 0.4..max * 0.65
-               [*output, 2]
-             when 1..max * 0.4
-               [*output, 1]
-             else
-               [*output, 0]
-             end
+# Set color level for each day
+a.each do |key, value|
+  case value
+  when max * 0.8..max
+    c.store(key, 4)
+  when max * 0.65..max * 0.8
+    c.store(key, 3)
+  when max * 0.4..max * 0.65
+    c.store(key, 2)
+  when 1..max * 0.4
+    c.store(key, 1)
+  else
+    c.store(key, 0)
   end
-  output = output.reverse if ENV['REVERSE']
 end
 
-puts "Color levels: #{output.join(' ')}"
-
-# Pass color levels to LifxLAN Python script
-puts `python3 lifx.py #{output.join(' ')}`
+puts c
+# Pass on activity to LIFX script
+puts `python3 lifx.py #{c.map { |_k, v| v }.join(' ')}`
